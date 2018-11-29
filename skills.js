369275bac9bd5ca0c9bcafed091f385e35f600b9
clem193 <natiezclement@gmail.com>
2018-11-30 00:04:31 +0100

    Update skills.js

--- skills.js
+++ skills.js
@@ -8,9 +8,10 @@ module.exports = {
 		131: {msg: 'Front -> Left Scratch -> Back'},//131 132 134
 		125: {msg: 'Front -> Right Scratch -> Back'},//125 126 127
 
-		108: {msg: 'Back Throw -> Front'},
-		111: {msg: 'Big hammer back'},
-		137: {msg: 'Hammer back '},
+		108: {msg: 'Stun'},
+		111: {msg: 'Back'},
+		112: {msg: 'Front'},
+		137: {msg: 'Back'},
 
 		114: {msg: 'Eviscerate'},
 		115: {msg: 'Knockup'},//114 115
@@ -18,18 +19,29 @@ module.exports = {
 
 		116: {msg: 'Donuts'},
 
-		139: {msg: 'Knockback (Bait)'},//108 110 139    118 139    101 105 107 139
+		101: {msg: 'Spin Patern'},
+		139: {msg: 'Spin'},//108 110 139    118 139    101 105 107 139
 
-		121: {msg: 'Waves (Left)'},
-		140: {msg: 'Waves (Right)'},
+		121: {msg: 'Normal Waves (Left)'},
+		122: {msg: '3rd Fast Waves (Left)'},
+		123: {msg: '2nd Fast Waves (Left)'},
+		
+		140: {msg: 'Normal Waves (Right)'},
+		141: {msg: '3rd Fast Waves (Right)'}, // ? WIP
+		142: {msg: '2nd Fast Waves (Right)'}, // ? WIP
 
 		135: {msg: 'Handle'},
 		112: {msg: 'Handle'},
+		
+		119: {msg: 'Left Swipe'},
+		120: {msg: 'Right Swipe'},
 
 		311: {msg: 'Wrath'},//311 120    311 119
 		312: {msg: 'Wrath'},//312 119    312 120
 
-		307: {msg0: 'Starting Bombs'},//307 301
+		307: {msg0: 'STUN IT!'},//307 301
+		309: {msg0: 'STUN IT!'},
+		310: {msg0: 'STUN IT!'},
 
 		305: {msg0: 'Meteors'},
 		306: {msg0: 'Meteors'},
